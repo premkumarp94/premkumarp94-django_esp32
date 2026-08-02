3dca6e58344714afd499f019a0e7e6b7d1bbe5b3
premkumarp94 <prex1994@gmail.com>
2026-08-02 15:03:14 +0530

    DJANGO_ESP32_2026_08_02_15_03_14

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path("", lambda req: redirect("/api/status/")),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-]
+]
